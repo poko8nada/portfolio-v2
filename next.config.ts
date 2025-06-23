3d68c57a5252af6360ed1681ab5739aff7f770ed
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-06-23 14:24:48 +0900

    Update wrangler configuration: set main worker file, adjust compatibility date, and add assets directory

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {}
-
-if (process.env.NODE_ENV === 'development') {
-  setupDevPlatform()
-}
 
 export default nextConfig
