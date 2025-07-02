ca725ee81c88dad2ebc1a7a8540a3ccac2d80d0c
poko8nada <57497035+poko8nada@users.noreply.github.com>
2025-07-02 14:47:29 +0900

    chore: update project files

--- src/app/(pages_layout)/about/page.tsx
+++ src/app/(pages_layout)/about/page.tsx
@@ -12,11 +12,14 @@ export default async function AboutPage() {
   return (
     <SectionBody>
       <div className='mt-16 mb-22 px-4 sm:px-0 w-full'>
-        <div className='w-full text-center'>
-          <p className='text-base md:text-md leading-relaxed text-fg/90'>
-            Webディレクター／デベロッパーとして、企画・設計から実装・運用まで一貫して担当。
-            <br className='hidden sm:inline' />
-            技術力と多様な業界経験、チームワーク・課題解決力を活かし、価値あるWeb体験をつくります。
+        <div className='w-full text-center md:px-4 px-0'>
+          <p className='text-base md:text-md leading-relaxed text-fg/90 mt-5'>
+            Next.js・React・TypeScriptを中心に幅広い技術をカバー。
+            <br />
+            Tailwind CSS・Drizzle ORMなどのフロントエンド、Node.js・Supabase・Cloudflareなどのバックエンド・クラウドまで構築できます。
+          </p>
+          <p className='text-base md:text-md leading-relaxed text-fg/90 mt-5'>
+            設計・実装から運用・分析まで、最適な技術で価値あるWeb体験を提供します。
           </p>
           <p className='mt-16 text-lg md:text-xl text-fg'>
             スキル一覧
